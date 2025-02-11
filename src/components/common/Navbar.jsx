5792e5f585e1f6e05f4b979327d6b93e03a2ddaa
rvedhas <rvedhas@gmail.com>
2025-02-11 23:52:20 +0530

    Update Navbarjsx

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from "../../assets/images/KishavSita.png"
 function Navbar() {
   return (
     <div>
-      <div className='h-[24px] flex border-b-[2px] border-black'>
+      <div className='h-[24px] flex border-b-[3px] border-black'>
         <Link to="./" >
         <img src={logo} alt="Logo" className='w-1/2 h-auto ml-[10px]' />
         </Link>
