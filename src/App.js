34313c263976f5540a5548087c4850fa703b5624
rvedhas <rvedhas@gmail.com>
2025-02-12 15:37:35 +0530

    Changes_12Feb

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import images from "./assets/images/2024_03_07 09_10 Office Lens.jpg";
 import Navbar from "./components/common/Navbar";
 
 function App() {
+  const title = "This is decalred varible"
   return (
     <main>
-      <Navbar></Navbar>
+      <Navbar title={title}></Navbar>
     </main>
       
     
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {Link}  from 'react-router-dom'
 import logo from "../../assets/images/KishavSita.png"
-function Navbar() {
+function Navbar({title}) {
+  
   return (
     <div>
       <div className='h-[24px] flex border-b-[3px] border-black'>
-        <Link to="./" >
-        <img src={logo} alt="Logo" className='w-1/2 h-auto ml-[10px]' />
-        </Link>
+        {/* <Link to="/" >
+        {/* <img src={logo} alt="Logo" className='w-1/2 h-auto ml-[10px]' /> */}
+        {/* {title} */}
+        {/* </Link> */} 
+        <p>{title}</p>
 
       </div>
     </div>
